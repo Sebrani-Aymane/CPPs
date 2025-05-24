2bd789e35c5b1bfb050ddfe7fed96736e2381b4f
aymane <sebraniayman@gmail.com>
2025-05-24 19:20:15 +0100

    segfault

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -19,4 +19,33 @@ Intern::~Intern() {}
 const char* Intern::NoSuchFormException::what() const throw() {
     return "Form name not found";
 }
-AForm* Intern::
+AForm* Intern::makeForm(std::string formName, std::string target) {
+    struct FormCreator {
+        std::string name;
+        AForm* (*create)(const std::string&);
+    };
+
+    FormCreator creators[3] = {
+        {"shrubbery creation", &Intern::createShrubbery},
+        {"robotomy request", &Intern::createRobotomy},
+        {"presidential pardon", &Intern::createPresidential}
+    };
+
+    for (int i = 0; i < 3; i++) {
+        if (formName == creators[i].name) {
+            std::cout << "Intern creates " << formName << std::endl;
+            return creators[i].create(target);
+        }
+    }
+    throw NoSuchFormException();
+}
+AForm* Intern::createShrubbery(const std::string& target) {
+    return new ShrubberyCreationForm(target);
+}
+AForm* Intern::createRobotomy(const std::string& target) {
+    return new RobotomyRequestForm(target);
+}
+AForm* Intern::createPresidential(const std::string& target) {
+    return new PresidentialPardonForm(target);
+}
+    
--- CPP05/ex03/Intern.hpp
+++ CPP05/ex03/Intern.hpp
@@ -4,24 +4,31 @@
 #include "AForm.hpp"
 #include "Bureaucrat.hpp"
 #include <string>
+#include <iostream>
 
-
-
-class Intern{
-    public :
-
+class Intern {
+public:
     Intern();
     ~Intern();
     Intern(const Intern &src);
     Intern& operator=(const Intern &rhs);
 
-    AForm* makeForm(std::string formName,std::string target);
-class NoSuchFormException : public std::exception{
+    AForm* makeForm(std::string formName, std::string target);
+
+    class NoSuchFormException : public std::exception {
     public:
-    virtual const char *what()const throw();
-};
+        const char* what() const throw();
+    };
 
-};
+    struct FormCreator {
+        std::string name;
+        AForm* (*create)(const std::string&);
+    };
 
+private:
+    static AForm* createShrubbery(const std::string& target);
+    static AForm* createRobotomy(const std::string& target);
+    static AForm* createPresidential(const std::string& target);
+};
 
 #endif
