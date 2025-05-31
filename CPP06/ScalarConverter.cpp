8f7529a3567aade42c17bbd7870213f92e3acd7b
aymane <sebraniayman@gmail.com>
2025-05-31 04:29:34 +0100

    chof kidir

---
+++ CPP06/ScalarConverter.cpp
@@ -0,0 +1,8 @@
+#include "ScalarConvert,.hpp"
+
+int ScalarConvert::getType(const sts::string &input)
+{
+    if(input.lenght()==3 && input[0]=='\'' && inputt[2]=='\'')
+        return 1;
+    if(input.atoi)
+}
